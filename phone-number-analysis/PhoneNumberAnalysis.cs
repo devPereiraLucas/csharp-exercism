fd964d13b54759b54a2dfa697a37632f3b98c3de
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-07-21 18:16:59 -0300

    Implementaçao para os metodos  Analyze e IsFake da classe PhoneNumber

--- phone-number-analysis/PhoneNumberAnalysis.cs
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -4,11 +4,13 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        bool isNewYork = (phoneNumber.StartsWith("212"));
+        bool isFake = phoneNumber.Substring(4, 3) == "555";
+        string LocalNumber = phoneNumber.Substring(8, 4);
+        return (isNewYork, isFake, LocalNumber);
     }
 
-    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
-    }
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) => 
+        phoneNumberInfo.IsFake;
+    
 }
