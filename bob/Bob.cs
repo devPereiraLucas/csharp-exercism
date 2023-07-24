04914f4ff6fa15aa00746c372756c6a3c3321f0e
“Lucas <lucaspereira.desenvolvedor@gmail.com>
2023-07-24 15:50:29 -0300

    Implementaçao dos metodos IsSilence, IsYelling e IsQuestion e seus testes

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Linq;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (statement.IsSilence())
+            return "Fine. Be that way!";
+
+        if (statement.IsYelling() && statement.IsQuestion())
+            return "Calm down, I know what I'm doing!";
+
+        if (statement.IsYelling())
+            return "Whoa, chill out!";
+
+        if (statement.IsQuestion()) return "Sure.";
+
+        return "Whatever.";
+
     }
+
+    private static bool IsQuestion(this string message)
+        => message.TrimEnd().EndsWith("?");
+
+    private static bool IsYelling(this string message)
+        => message.Any(char.IsLetter) && message.ToUpperInvariant() == message;
+
+    private static bool IsSilence(this string message)
+        => string.IsNullOrWhiteSpace(message);
+    
 }
